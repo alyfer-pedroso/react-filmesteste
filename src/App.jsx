d4453e38f4d2d6aae4beb0017510425fa2e147ab
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-12-28 23:19:32 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Movies from "./pages/Movies";
+import Footer from "./components/Footer";
 
 import "./App.css";
 
@@ -14,6 +15,7 @@ function App() {
         <Route exact path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
       </Routes>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <p> Site é apenas um agregador de links, nenhum arquivo está hospedado sob nosso domínio.</p>
+  );
+};
+
+export default Footer;
