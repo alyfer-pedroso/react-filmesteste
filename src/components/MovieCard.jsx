39f3fe6ab48d3aacfe639b309ceec8efc21644a6
Alyfer Pedroso <alyferleandroaraujopedroso@gmail.com>
2023-12-28 23:23:42 -0300

    commit

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,7 +1,15 @@
 import React from "react";
 
 const MovieCard = ({ link }) => {
-  return <iframe src={link} frameborder="0"></iframe>;
+  return (
+    <iframe
+      src={link}
+      frameborder="0"
+      allowfullscreen="true"
+      webkitallowfullscreen="true"
+      mozallowfullscreen="true"
+    ></iframe>
+  );
 };
 
 export default MovieCard;
